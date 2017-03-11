93ae2fe6dd5a8acba7be50784f35ef40fa648105
Paul Mendez <pmendez@ing-mat.udec.cl>
2017-03-10 22:22:33 -0300

    Major refactor to make easy add extra schemes

---
+++ src/example_burger.f90
@@ -0,0 +1,84 @@
+MODULE example_burger
+USE decimal
+USE plot
+USE tipos
+USE numeric_schemes
+IMPLICIT NONE
+CONTAINS
+subroutine burger_runexample(initial_condition)
+  INTEGER                   :: initial_condition
+  REAL(kind = dp)           :: Tend
+  INTEGER                   :: N
+  REAL(kind = dp)           :: dx
+  REAL(kind = dp)           :: CFL
+  REAL(kind = dp)           :: dt
+  REAL(kind = dp)           :: mu
+  INTEGER                   :: ntime
+  REAL(kind = dp), ALLOCATABLE    :: xx(:)
+  REAL(kind = dp), ALLOCATABLE    :: uinit(:), uu(:), uold(:)
+  REAL(kind = dp), ALLOCATABLE    :: fplus(:), fminus(:), KK(:)
+  INTEGER                   :: tt, i, j
+  REAL(kind = dp), ALLOCATABLE    :: uleft, uright, fplusleft, fminusright
+  REAL(kind = dp), ALLOCATABLE    :: Kleft, Kright
+  CHARACTER(LEN=32)               :: name           ! File name to save plot data
+
+  ! Initialize variables
+  Tend = 0.5_dp      ! Final Time
+  N = 400          ! Number of nodes
+  CFL = 0.9_dp
+  mu = 0.01
+  dx = 4.0_dp/(N-1)
+  dt = CFL/(1.0_dp*(1/dx+4*mu/dx**2))
+  ntime = floor(tend/dt)  !Number of time steps
+
+    ! Allocate memory
+  ALLOCATE(xx(N), uu(N))
+  xx = 0.0_dp; uu = 0.0_dp
+  xx = (/(i*dx-dx-2,i=1, N)/)      ! Location of grid points
+
+  ! Initial Conditions
+  IF (initial_condition == 1) THEN
+    DO j = 1, N
+      IF (xx(j) > -1 .AND. xx(j) < 1) THEN
+        uu(j) = (1 - xx(j)**2)**2
+      ELSE
+        uu(j) = 0.0_dp
+      END IF
+    END DO
+  elseif (initial_condition == 2) then    
+    DO j = 1, N
+      IF (xx(j) > -0.5 .AND. xx(j) < 0.5) THEN
+        uu(j) = 1
+      ELSE
+        uu(j) = 0.0_dp
+      END IF
+    END DO
+  else
+    uu = 0.0_dp
+  END IF
+  !Save initial condition
+  ALLOCATE(uinit(N))
+  uinit = 0.0_dp;   
+  uinit = uu
+
+  !Engquist-Osher Scheme with forward Euler
+  name = 'output'
+  CALL Engquist_Osher(FORWARD_EULER, uu, N, ntime, dx, dt, flux, DiffMat)
+  CALL plot_results(uu, uinit, xx, name)
+end subroutine burger_runexample
+
+FUNCTION flux(uu) RESULT(ff)
+  !Flux in Burguer's Equation
+  REAL(kind = dp), INTENT(IN)  :: uu
+  REAL(kind = dp)              :: ff
+  ff = 0.5*uu**2
+END FUNCTION flux
+
+FUNCTION DiffMat(uu, mu) RESULT(kk)
+  !Conservative diffusion in Burguer's Equation
+  REAL(kind = dp), INTENT(IN)  :: uu
+  REAL(kind = dp)              :: kk
+  REAL(kind = dp), INTENT(IN)  :: mu
+  kk = mu*uu**2
+END FUNCTION DiffMat
+END MODULE example_burger
---
+++ src/numeric_schemes.f90
@@ -0,0 +1,52 @@
+MODULE numeric_schemes
+USE decimal
+USE tipos
+IMPLICIT NONE
+CONTAINS
+subroutine Engquist_Osher(time_scheme, uu, N, ntime, dx, dt, Flux, DiffMat)
+  INTEGER                   :: time_scheme
+  INTEGER                   :: N
+  REAL(kind = dp)           :: dx
+  REAL(kind = dp)           :: dt
+  REAL(kind = dp)           :: mu
+  INTEGER                   :: ntime
+  REAL(kind = dp), ALLOCATABLE    :: uu(:), uold(:)
+  REAL(kind = dp), ALLOCATABLE    :: fplus(:), fminus(:), KK(:)
+  INTEGER                   :: tt, i, j
+  REAL(kind = dp), ALLOCATABLE    :: uleft, uright, fplusleft, fminusright
+  REAL(kind = dp), ALLOCATABLE    :: Kleft, Kright
+  REAL(kind = dp),external        :: Flux, DiffMat
+  
+  uleft = uu(1); uright = uu(N)
+  ALLOCATE(fplus(N), fminus(N), uold(N), KK(N))
+  fplus = 0.0_dp;   fminus = 0.0_dp
+  uold = 0.0_dp; KK = 0.0_dp
+  DO tt = 1,ntime
+    uold = uu
+    DO j = 1,N
+      IF (uold(j) > 0) THEN
+        fplus(j) = Flux(uold(j)); fminus(j) = 0.0_dp
+      ELSE
+        fplus(j) = 0.0_dp; fminus(j) = Flux(uold(j))
+      END IF
+      KK(j) = DiffMat(uold(j), mu)
+    END DO
+    fplusleft = Flux(uleft); fminusright = Flux(uright)
+    Kleft = DiffMat(uleft, mu); Kright = DiffMat(uright, mu)
+
+    !Engquist-Osher Scheme with forward Euler
+    IF (time_scheme == FORWARD_EULER) THEN
+      j = 1
+      uu(j) = uold(j) - dt/dx * (fplus(j) + fminus(j+1) - fplusleft-fminus(j)) +&
+      dt/dx**2*(KK(j+1) - 2*KK(j) + Kleft)
+      DO j = 2,(N-1)
+        uu(j) = uold(j) - dt/dx * (fplus(j) + fminus(j+1) - fplus(j-1)-fminus(j)) +&
+        dt/dx**2*(KK(j+1) - 2*KK(j) + KK(j-1))
+      END DO
+      j = N
+      uu(j) = uold(j) - dt/dx * (fplus(j) + fminusright - fplus(j-1)-fminus(j)) +&
+      dt/dx**2*(Kright - 2*KK(j) + KK(j-1))
+    END IF
+  END DO
+end subroutine Engquist_Osher
+END MODULE numeric_schemes
